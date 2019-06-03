37f24327f79d6b2f46a53adcc3f5b5eaef14013d
Xavier Goby <xgoby@hotmail.com>
2019-06-03 18:40:34 +0200

    Added not yet implemented oop version of predictor script

---
+++ predicting/OOP_Predicter.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 09/10/2018
+
+@author: Xavier O'Rourke Goby
+"""
+import cv2
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
+
+class PredictionProducer:
+
+    def __init__(self, ):
